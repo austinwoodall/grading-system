406a525f5d51582e518e88698e8816334a11cf33
austinwoodall <austin.woodall72@gmail.com>
2017-09-08 18:18:45 -0500

    new changes

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var input = document.getElementById('input');
 var grade = document.getElementsByClassName('grade');
+var gradeList = document.getElementsByTagName('li');
 
 var span = "";
 var gradeArray = [];
 var average = 0;
 var sumArr = 0;
 
+for(var i = 0; i < gradeList; i++) {
+   var createdSpan = document.createElement('span');
+   span.className('grade-letter');
+}
+
 function calculateGrade() {
+   var li = document.createElement('li');
+   var gradeValue = document.getElementById('input').value;
+   var g = document.createTextNode('Grade: ');
+   li.appendChild(g);
+   
+   document.getElementById('grades-list').appendChild('li');
+   document.getElementById('input').value = '';
+
    var inputGrade = input.value;
    for (var i = 0; i < inputGrade.length; i++) {
       if (inputGrade <= 100) {
@@ -29,11 +43,12 @@ function calculateGrade() {
    document.getElementById('grade-letter').innerHTML = span;
    document.getElementById('average').innerHTML = average; 
 
+   // Adds numbers inside the array
    for(i = 0; i < gradeArray.length; i++) {
       sumArr += +gradeArray[i];
       console.log(sumArr);
    }
-
+  // values over 100 cannot be inputted
    if(inputGrade > 100) {
       alert("Invalid Grade Value");
       gradeArray.slice();
@@ -41,15 +56,14 @@ function calculateGrade() {
    else {
       gradeArray.push(inputGrade);
    }
-
+  // Average doesnt go over 100
    if(average >= 100) {
       average = 100;
    }
    else {
+   // Divides sum of array then divides by the amount of numbers in array
       average = sumArr / gradeArray.length;
    }
-   
-   
    console.log(gradeArray);
 }
 
