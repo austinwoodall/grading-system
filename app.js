4e2bdf39e0a5b1683d458fcf74508fadc87e886c
austinwoodall <austin.woodall72@gmail.com>
2017-09-08 17:31:21 -0500

    made changes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var input = document.getElementById('input');
 var grade = document.getElementsByClassName('grade');
 
 var span = "";
+var gradeArray = [];
+var average = 0;
+var sumArr = 0;
 
 function calculateGrade() {
    var inputGrade = input.value;
@@ -9,24 +12,47 @@ function calculateGrade() {
       if (inputGrade <= 100) {
          span = "A";
       }
-
       if (inputGrade <= 89) {
          span = "B";
       }
-
       if(inputGrade <= 79) {
          span = "C";
       }
-
       if(inputGrade <= 69) {
          span = "D";
       }
-
       if(inputGrade <= 59) {
          span = "F";
       }
-   
-      document.getElementById('grade-letter').innerHTML = span;
    }
+
+   document.getElementById('grade-letter').innerHTML = span;
+   document.getElementById('average').innerHTML = average; 
+
+   for(i = 0; i < gradeArray.length; i++) {
+      sumArr += +gradeArray[i];
+      console.log(sumArr);
+   }
+
+   if(inputGrade > 100) {
+      alert("Invalid Grade Value");
+      gradeArray.slice();
+   }
+   else {
+      gradeArray.push(inputGrade);
+   }
+
+   if(average >= 100) {
+      average = 100;
+   }
+   else {
+      average = sumArr / gradeArray.length;
+   }
+   
+   
+   console.log(gradeArray);
 }
+
+
+
 
