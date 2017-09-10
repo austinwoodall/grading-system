4030ca54ee201c627460362da1118b5e8cd9b7e8
austinwoodall <austin.woodall72@gmail.com>
2017-09-10 17:28:46 -0500

    new changes

--- app.js
+++ app.js
@@ -1,72 +1,56 @@
 var input = document.getElementById('input');
 var grade = document.getElementsByClassName('grade');
-var gradeList = document.getElementsByTagName('li');
+
+
 
 var span = "";
 var gradeArray = [];
 var average = 0;
 var sumArr = 0;
 
-for(var i = 0; i < gradeList; i++) {
-   var createdSpan = document.createElement('span');
-   span.className('grade-letter');
-}
-
 function calculateGrade() {
-   var li = document.createElement('li');
-   var gradeValue = document.getElementById('input').value;
-   var g = document.createTextNode('Grade: ');
-   li.appendChild(g);
    
-   document.getElementById('grades-list').appendChild('li');
-   document.getElementById('input').value = '';
-
+   var gradeValue = document.getElementById('input').value;
    var inputGrade = input.value;
-   for (var i = 0; i < inputGrade.length; i++) {
-      if (inputGrade <= 100) {
-         span = "A";
-      }
-      if (inputGrade <= 89) {
-         span = "B";
-      }
-      if(inputGrade <= 79) {
-         span = "C";
-      }
-      if(inputGrade <= 69) {
-         span = "D";
-      }
-      if(inputGrade <= 59) {
-         span = "F";
-      }
+
+   if (inputGrade <= 100) {
+      span = "A";
+   }
+   if (inputGrade <= 89) {
+      span = "B";
+   }
+   if (inputGrade <= 79) {
+      span = "C";
+   }
+   if (inputGrade <= 69) {
+      span = "D";
+   }
+   if (inputGrade <= 59) {
+      span = "F";
    }
 
+   // Hook up <elements> to their corresponding variables
    document.getElementById('grade-letter').innerHTML = span;
-   document.getElementById('average').innerHTML = average; 
+   document.getElementById('average').innerHTML = average;
 
-   // Adds numbers inside the array
-   for(i = 0; i < gradeArray.length; i++) {
+   document.getElementById('input').value = '';
+
+   // Gets sum of numbers inside the array
+   for (i = 0; i < gradeArray.length; i++) {
       sumArr += +gradeArray[i];
       console.log(sumArr);
    }
-  // values over 100 cannot be inputted
-   if(inputGrade > 100) {
+   // values over 100 and blnk values are ignored
+   if (inputGrade > 100 || inputGrade == '') {
       alert("Invalid Grade Value");
       gradeArray.slice();
    }
    else {
       gradeArray.push(inputGrade);
    }
-  // Average doesnt go over 100
-   if(average >= 100) {
-      average = 100;
-   }
-   else {
+
    // Divides sum of array then divides by the amount of numbers in array
-      average = sumArr / gradeArray.length;
-   }
+   average = sumArr / gradeArray.length;
+
    console.log(gradeArray);
 }
-
-
-
-
